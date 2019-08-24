74200ee6387721c0aea40a5e3df5ef04293ca955
Adriana Sturlan <adriana.sturlan@convergent-usa.com>
2019-08-24 10:39:40 +0200

    Year and Annual routes

--- src/api/v1/annual/index.js
+++ src/api/v1/annual/index.js
@@ -12,11 +12,24 @@ router.get(api.ANNUAL, async (req,res)=>{
         sendErrorResponce(res,ex)
     }
 })
-router.post(api.ANNUAL,(req,res)=>{
-    AnnualModel.findOrCreate({
-        where: req.body
-    }).then(
-        res.status(200).send('Database updated')
-    )
+router.post(api.ANNUAL, async (req,res)=>{
+    try{
+        const ANNUAL=await AnnualModel.create(req.body)
+        res.status(200).json(ANNUAL)
+    }catch(ex){
+        sendErrorResponce(res,ex);
+    }
+})
+router.delete(api.ANNUAL, (req,res) => {
+    req.body.forEach(id => {
+        try{
+            AnnualModel.destroy({
+                where: {_id : id}
+            })
+        }catch(ex){
+            sendErrorResponce(res,ex);
+        }
+    })
+    res.status(200).send('Deleted')
 })
 module.exports = router;
